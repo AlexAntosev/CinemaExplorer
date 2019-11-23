83d994a12645fa618b8c2b3a9fc36777d4517a2b
Alex Antosev <alexander.antosev@gmail.com>
2019-11-24 01:13:32 +0200

    Implemented api for getting films by list of ids

--- CinemaExplorer.Business/Interfaces/IFilmService.cs
+++ CinemaExplorer.Business/Interfaces/IFilmService.cs
@@ -1,8 +1,12 @@
 ﻿using CinemaExplorer.Persisted.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CinemaExplorer.Business.Interfaces
 {
     public interface IFilmService : IGenericService<Film>
     {
+        Task<List<Film>> GetAllByIds(List<Guid> ids);
     }
 }
